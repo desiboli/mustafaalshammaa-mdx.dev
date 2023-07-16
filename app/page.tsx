d30c94e5eb18ef8d265069e7ff3774e1bd54f71e
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2023-07-16 21:45:50 +0200

    title style

--- app/page.tsx
+++ app/page.tsx
@@ -3,31 +3,35 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col lg:flex-row justify-between gap-10">
-      <div className="flex-1">
-        <h1 className="text-2xl font-semibold">Hey! I'm Musse</h1>
-        <p>
-          My name is Mustafa Alshammaa, a Senior Frontend Developer and the
-          Co-founder of{" "}
-          <Button variant="link" className="p-0" asChild>
-            <a href="https://pluckmarket.io/" className="text-base">
-              Pluck
-            </a>
-          </Button>
-          . This is my platform for sharing thoughts and lessons learned along
-          the way.
-        </p>
-      </div>
+    <>
+      <section>
+        <div className="flex flex-col lg:flex-row justify-between gap-10">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold mb-4">Hey! I'm Musse</h1>
+            <p>
+              My name is Mustafa Alshammaa, a Senior Frontend Developer and the
+              Co-founder of{" "}
+              <Button variant="link" className="p-0" asChild>
+                <a href="https://pluckmarket.io/" className="text-base">
+                  Pluck
+                </a>
+              </Button>
+              . This is my platform for sharing thoughts and lessons learned
+              along the way.
+            </p>
+          </div>
 
-      <div className="relative w-64 h-64 max-w-full rounded-xl overflow-hidden">
-        <Image
-          src="/me.jpg"
-          alt="Me"
-          fill
-          style={{ objectFit: "cover" }}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-      </div>
-    </div>
+          <div className="relative w-64 h-64 max-w-full rounded-xl overflow-hidden">
+            <Image
+              src="/me.jpg"
+              alt="Me"
+              fill
+              style={{ objectFit: "cover" }}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
